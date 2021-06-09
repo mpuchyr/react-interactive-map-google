85b93e82fcb1e63c75ae3682579959b4625b2e41
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-06-09 14:34:50 -0500

    Create InfoWindow popup when clicking on a marker in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import './App.css';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import React, { useState } from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import data from './data/chicago-parks.json';
 
-const Map = () => {
-  return (
-    <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 41.8781, lng: -87.6298 }}
-    />
-  )
-}
 
-const WrappedMap = withScriptjs(withGoogleMap(Map))
+
 
 function App() {
+  const [selectedPark, setSelectedPark] = useState(null)
+
+  const displayMarkers = () => {
+    return data.map((park, index) => {
+      if (park.location.hasOwnProperty('latitude') && park.location.hasOwnProperty('longitude')) {
+        const lat = parseFloat(park.location.latitude)
+        const lng = parseFloat(park.location.longitude)
+        return (
+          <Marker
+            key={index}
+            position={{ lat: lat, lng: lng}}
+            onClick={() => {
+              setSelectedPark(park)
+            }}
+          ></Marker>
+        )
+      }
+    })
+  }
+  
+  const Map = () => {
+    return (
+      <GoogleMap
+        defaultZoom={10}
+        defaultCenter={{ lat: 41.8781, lng: -87.6298 }}
+      >
+        {displayMarkers()}
+        {selectedPark && (
+          <InfoWindow
+            position={{lat: parseFloat(selectedPark.location.latitude), lng: parseFloat(selectedPark.location.longitude)}}
+            onCloseClick={() => setSelectedPark(null)}
+          >
+            <div>
+              <h2>{selectedPark.park}</h2>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    )
+  }
+  
+  const WrappedMap = withScriptjs(withGoogleMap(Map))
+
+  
   return (
     <div style={{ width: "100vw", height: "100vh"}}>
       <WrappedMap 
